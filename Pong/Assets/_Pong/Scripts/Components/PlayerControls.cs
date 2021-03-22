dad75f6c99e0f57cc7ca7f50f10721598dd483ff
srodney <stevenrodney@hotmail.com>
2021-03-22 21:02:47 +0000

    Altered paddle code to use Unity input manager.

--- Pong/Assets/_Pong/Scripts/Components/PlayerControls.cs
+++ Pong/Assets/_Pong/Scripts/Components/PlayerControls.cs
@@ -3,67 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private static string PaddleOneOwner => "PLAYER";
-    private static string PaddleTwoOwner => "OPPONENT";
+    private static readonly float InitialSpeed = 10.0f;
+    private static readonly float BoundY = 2.25f;
+    private static readonly string PaddleOneTagName = "PLAYER";
+    private static readonly string PaddleTwoTagName = "OPPONENT";
 
-    private static float InitialSpeed = 10.0f;
-    private static float InitialBoundY = 2.25f;
-
-    private KeyCode moveUp = KeyCode.W;
-    private KeyCode moveDown = KeyCode.S;
-    private float speed = InitialSpeed;
-    private float boundY = InitialBoundY;
-
-    private Rigidbody2D rigidBody2d = null;
+    private string axis = null;
+    private Rigidbody2D paddle = null;
 
     void Start()
     {
-        rigidBody2d = GetComponent<Rigidbody2D>();
-
-        if(rigidBody2d != null)
+        paddle = GetComponent<Rigidbody2D>();
+        string paddleTag = paddle.tag;
+        if(paddleTag.ToUpper() == PaddleOneTagName)
         {
-            string tagName = rigidBody2d.tag.ToUpper();
-            if (tagName == PaddleTwoOwner)
-            {
-                moveUp = KeyCode.UpArrow;
-                moveDown = KeyCode.DownArrow;
-            }
+            axis = "Vertical";
+        }
+        else if(paddleTag.ToUpper() == PaddleTwoTagName)
+        {
+            axis = "Vertical2";
         }
     }
 
     // FixedUpdate is called once per fixed time interval, Use with any component that has physics
     void FixedUpdate()
     {
-        if(rigidBody2d != null)
+        float paddleVelocity = Input.GetAxisRaw(axis);
+        paddle.velocity = new Vector2(0.0f, paddleVelocity) * InitialSpeed;
+
+        Vector3 currentPosition = transform.position;
+        if(currentPosition.y > BoundY)
         {
-            var vel = rigidBody2d.velocity;
-            if (Input.GetKey(moveUp)) 
-            {
-                vel.y = speed;
-            }
-            else if (Input.GetKey(moveDown))
-            {
-                vel.y = -speed;
-            }
-            else 
-            {
-                vel.y = 0;
-            }
-
-            rigidBody2d.velocity = vel;
-
-            var pos = transform.position;
-            if (pos.y > boundY) 
-            {
-                pos.y = boundY;
-            }
-            else if (pos.y < -boundY) 
-            {
-                pos.y = -boundY;
-            }
-            
-            transform.position = pos;
+            currentPosition.y = BoundY;
         }
+        else if (currentPosition.y < -BoundY) 
+        {
+            currentPosition.y = -BoundY;
+        }
+        
+        transform.position = currentPosition;
     }
 }
